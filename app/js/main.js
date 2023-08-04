a7f844d3fb63bd33ba8179db908099f3c4cba647
Ihor Troianovskyi <igtroya555@gmail.com>
2023-08-04 13:37:28 +0300

    tabs

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,21 @@
     headerNavigation.classList.remove("active");
   });
 })();
+
+(() => {
+  const tabsItems = document.querySelectorAll(".tabs__list-item");
+  const contentsItems = document.querySelectorAll(".tabs__content-item");
+
+  tabsItems.forEach((tab, index) => {
+    tab.addEventListener("click", (e) => {
+      contentsItems.forEach((content) => {
+        content.classList.remove("active");
+      });
+      tabsItems.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+      tabsItems[index].classList.add("active");
+      contentsItems[index].classList.add("active");
+    });
+  });
+})();
